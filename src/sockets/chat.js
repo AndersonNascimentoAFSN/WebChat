c5e5b82ba7753d4db7f94ac474d84059dcc167b1
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-30 00:52:12 -0300

    feat:back-end - add script socket.io

---
+++ src/sockets/chat.js
@@ -0,0 +1,29 @@
+const { io } = require('../api/http');
+const sendMessage = require('../utils/sendMessage');
+
+const users = {};
+
+io.on('connection', (socket) => {
+  console.log(`Client ${socket.id} connected`);
+  users[socket.id] = socket.id.slice(0, 16);
+
+  socket.on('nickname', (nickname) => {
+    users[socket.id] = nickname;
+
+    socket.emit('nickname', users[socket.id]);
+    io.emit('listUsers', Object.values(users));
+  });
+
+  socket.on('message', async ({ nickname, chatMessage, socketId = '' }) => {
+    await sendMessage({ socketId, nickname, chatMessage, io, users });
+  });
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    console.log(`Client ${socket.id} disconnected`);
+    io.emit('listUsers', Object.values(users));
+  });
+
+  socket.emit('nickname', users[socket.id]);
+  io.emit('listUsers', Object.values(users));
+});
