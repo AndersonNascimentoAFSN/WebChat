c50c4ee596e373a4189ba512c1998c9dc6fb2d53
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-30 01:10:55 -0300

    fix: back-end - change function model for createMessages

--- src/utils/sendMessage.js
+++ src/utils/sendMessage.js
@@ -5,13 +5,13 @@ module.exports = async ({ socketId, nickname, chatMessage, io, users }) => {
   if (!socketId) {
     const timestamp = moment().format('DD-MM-YYYY HH:mm:ss A');
     const message = `${timestamp} - ${nickname}: ${chatMessage}`;
-    await postsModel.createPosts({ chatMessage, nickname });
+    await postsModel.createMessages({ chatMessage, nickname });
     io.emit('message', message);
     return;
   }
   const nickName = users[socketId];
   const timestamp = moment().format('DD-MM-YYYY HH:mm:ss A');
   const message = `${timestamp} - ${nickName}: ${chatMessage}`;
-  await postsModel.createPosts({ chatMessage, nickname });
+  await postsModel.createMessages({ chatMessage, nickname });
   io.emit('message', message);
 };
